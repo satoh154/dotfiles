ac13017ca3f81c4e4442012afe0565745151756a
satoh <satohka.111@gmail.com>
2020-04-28 23:04:10 +0900

    Add alias for neovim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,9 @@ set -x PATH /home/satoh/anaconda3/bin $PATH
 set -g theme_display_user no 
 set -g theme_display_hostname no
 alias nvim='/home/satoh/.local/bin/nvim.appimage'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/satoh/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
